812a4c2cc1238e2e51ae2e1706ad1ddada0ce16d
lesotropyx <90562367+lesotropyx@users.noreply.github.com>
2023-03-28 13:57:59 +0300

    laboratornaya1 Kokhanskaya Alena

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
+#include <string>
 #include "fun.h"
 
 int main() {
+    char str[1000];
+    std::cin.getline(str, 1000);
+    std::cout << "Words without numbers: " << faStr1(str) << std::endl;
+    std::cout << "Words with a capital: " << faStr2(str) << std::endl;
+    std::cout << "Average word length: " << faStr3(str) << std::endl;
     return 0;
 }
